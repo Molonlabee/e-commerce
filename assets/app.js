41115c7c96dc3d4e46ff08135b35361a9fa70faf
Molonlabee <janayahernandez@yahoo.com>
2022-04-26 21:09:51 -0400

    get the look

--- assets/app.js
+++ assets/app.js
@@ -354,3 +354,25 @@ mobileModeOn();
 window.addEventListener('resize', function (event) {
     mobileModeOn();
 })
+
+// EXTRA CREDIT
+// LOOK BOOK
+var slideIndex =1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+  showSlides(slideIndex += n)
+}
+function currentSlide(n){
+  showSlides(slideIndex = n)
+}
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName('slide-box');
+  if (n > slides.length){slideIndex = 1}
+  if(n < 1){slideIndex = slides.length}
+  for (i =0; i< slides.length; i++){
+    slides[i].style.display = 'none'
+}
+  slides[slideIndex -1].style.display = 'block';
+}
